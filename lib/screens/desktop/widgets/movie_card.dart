a1da43b8e1c894b3d459361819b080c57cfc07bd
Erick Namukolo <erickmndev@gmail.com>
2022-05-20 18:33:48 +0200

    FIXED: search bug

--- lib/screens/desktop/widgets/movie_card.dart
+++ lib/screens/desktop/widgets/movie_card.dart
@@ -37,86 +37,85 @@ class _MovieCardState extends State<MovieCard> {
           _isHovered = false;
         });
       },
-      child: GestureDetector(
-        onTap: () async {
-          Provider.of<MovieViews>(context, listen: false)
-              .toggleMovieDetails(true);
-          Provider.of<MovieViews>(context, listen: false).toggleisSearch(false);
-          Provider.of<Movies>(context, listen: false)
-              .fetchMovieDetails(widget.movie.id);
-          await Provider.of<Movies>(context, listen: false)
-              .clearRelatedMovies();
-          Provider.of<Movies>(context, listen: false)
-              .fetchRelatedMovies(widget.movie.id);
-        },
-        child: AnimatedContainer(
-          duration: const Duration(milliseconds: 200),
-          curve: Curves.easeIn,
-          margin: EdgeInsets.only(right: widget.isGrid ? 0 : 20),
-          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-          decoration: BoxDecoration(
-            color: kSecondaryColorDark,
-            borderRadius: BorderRadius.circular(10.0),
-            image: DecorationImage(
-              image: NetworkImage(widget.movie.coverImg),
-              fit: BoxFit.cover,
-              colorFilter: ColorFilter.mode(
-                Colors.black.withOpacity(0.5),
-                BlendMode.overlay,
+      child: Consumer<Movies>(
+        builder: (context, mov, __) => GestureDetector(
+          onTap: () async {
+            Provider.of<MovieViews>(context, listen: false)
+                .toggleMovieDetails(true);
+            mov.fetchMovieDetails(widget.movie.id);
+            await mov.clearRelatedMovies();
+            mov.fetchRelatedMovies(widget.movie.id);
+          },
+          child: AnimatedContainer(
+            duration: const Duration(milliseconds: 200),
+            curve: Curves.easeIn,
+            margin: EdgeInsets.only(right: widget.isGrid ? 0 : 20),
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            decoration: BoxDecoration(
+              color: kSecondaryColorDark,
+              borderRadius: BorderRadius.circular(10.0),
+              image: DecorationImage(
+                image: NetworkImage(widget.movie.coverImg),
+                fit: BoxFit.cover,
+                colorFilter: ColorFilter.mode(
+                  Colors.black.withOpacity(0.5),
+                  BlendMode.overlay,
+                ),
               ),
             ),
-          ),
-          width: _isHovered ? 190 : 200,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10.0),
-                      color: kAccentColor.withOpacity(.5),
-                    ),
-                    child: Center(
-                      child: Text(
-                        widget.movie.rating.toString(),
-                        style: kBodyTextStyleWhite.copyWith(fontSize: 10),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              const Spacer(),
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 4),
-                decoration: BoxDecoration(
-                  color: kSecondaryColorDark.withOpacity(.8),
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                child: Column(
+            width: _isHovered ? 190 : 200,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(
-                      widget.movie.title,
-                      style: kBodyTextStyleWhite,
-                    ),
-                    const SizedBox(height: 5),
-                    Text(
-                      widget.movie.year.toString(),
-                      style: kBodyTextStyleGrey.copyWith(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 12,
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 8, vertical: 4),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                        color: kAccentColor.withOpacity(.5),
+                      ),
+                      child: Center(
+                        child: Text(
+                          widget.movie.rating.toString(),
+                          style: kBodyTextStyleWhite.copyWith(fontSize: 10),
+                        ),
                       ),
                     ),
                   ],
                 ),
-              ),
-            ],
+                const Spacer(),
+                Container(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 6, vertical: 4),
+                  decoration: BoxDecoration(
+                    color: kSecondaryColorDark.withOpacity(.8),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        widget.movie.title,
+                        style: kBodyTextStyleWhite,
+                      ),
+                      const SizedBox(height: 5),
+                      Text(
+                        widget.movie.year.toString(),
+                        style: kBodyTextStyleGrey.copyWith(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 12,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
