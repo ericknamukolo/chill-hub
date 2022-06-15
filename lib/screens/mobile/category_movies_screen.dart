5e3b1863225556af73f32be894671826fc666f88
Erick Namukolo <erickmndev@gmail.com>
2022-06-15 18:47:01 +0200

    added a category movies screen

---
+++ lib/screens/mobile/category_movies_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CategoryMovieScreen extends StatelessWidget {
+  const CategoryMovieScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
